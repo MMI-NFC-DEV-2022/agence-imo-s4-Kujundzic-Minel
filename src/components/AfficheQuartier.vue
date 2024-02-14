66abafaafee6a4dc971be49af5c1826b2850cdd2
Minel <Minel@example.com>
2024-02-14 10:52:54 +0100

    edit fonctionnel

---
+++ src/components/AfficheQuartier.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Icon1 from '@/components/icons/Icon-1.vue';
+
+import { defineProps } from 'vue';
+
+import { supabase } from '@/supabase'
+import { type Database } from '@/supabase-types';
+
+</script>
+
+
+<template>
+            <hr/>
+            <div class="w-full justify-between flex">
+                <div class="flex py-4 gap-4"><Icon1/> {{nomQuartier}} Commune</div>
+            </div>
+</template>
---
+++ src/components/FormulaireEditOffreQuartier.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import AfficheMaison from '@/components/AfficheQuartier.vue'
+import { supabase } from '@/supabase'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+const quartier = ref({})
+
+async function upsertQuartier(dataForm, node) {
+  const { data, error } = await supabase.from('quartier').upsert(dataForm).select()
+  if (error) {
+    node.setErrors([error.message])
+  } else {
+    node.setErrors([])
+    console.log("id : ",data[0].id);
+    
+    // Mise à jour du nom de route et paramètres dans router.push
+    router.push({ name: "/quartier/edit/[[id]]", params: { id: data[0].id } });
+  }
+}
+
+if (route.params.id) {
+ // On charge les données de la maison
+ let { data, error } = await supabase
+ .from("quartier")
+ .select("*")
+ .eq("id", route.params.id);
+ if (error) console.log("n'a pas pu charger le table quartier :", error);
+ else quartier.value = (data as any[])[0];
+}
+
+console.log("quartier : ",quartier.value);
+</script>
+
+<template>
+  <div>
+    <div class="p-2">
+      <h2 class="text-2xl">Résultat (Prévisualisation)</h2>
+      <AfficheQuartier v-bind="quartier" />
+    </div>
+    <div class="p-2">
+      <FormKit
+        type="form"
+        v-model="quartier"
+        @submit="upsertQuartier"
+        :config="{
+          classes: {
+            input: 'p-1 rounded border-gray-300 shadow-sm border',
+            label: 'text-gray-600'
+          }
+        }"
+        :submit-attrs="{ classes: { input: 'bg-red-300 p-1 rounded' } }"
+      >
+        <FormKit name="nomQuartier" label="Nom Quartier"/>
+      </FormKit>
+    </div>
+  </div>
+</template>
